318af8a915bd504f58aa2479cb06318c8c72e912
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-20 21:12:57 +0300

    Add files via upload

---
+++ 3.1 dr hw/main.py
@@ -0,0 +1,27 @@
+# def func1(text):
+#  res1 =min(text)
+#  res2 =max(text)
+#  return res1,res2
+# result1  = func1(text=inp1)
+# print(result1)
+# inp1 = [2,4,6,3,7,8,5,1]
+
+
+
+# 2
+# inp2= input("text kiriting: ")
+# def long1 (text=str):
+#     res3 = len(text)
+#     return res3
+# result2 = long1(text=inp2)
+# print(result2)
+
+
+# inp3 = input("text kiriting: ")
+# def proverka (text2):
+#     if text2[-9:]=="gmail.com":
+#         print('Siz mars space ga kirdingiz')
+#     else:
+#         print("siz tizimga kirmadingiz error")
+#     return text2
+# result3 = proverka(text2=inp3)
